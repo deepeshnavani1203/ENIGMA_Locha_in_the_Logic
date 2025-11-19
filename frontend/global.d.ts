ebebd692f64f7bb894ced711a364d31587592f0e
Deepesh <deepeshnavani@gmail.com>
2025-11-19 19:43:54 +0530

    card changed

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -2,17 +2,17 @@
 // By augmenting the global JSX namespace, we can use <ion-icon> in our TSX files without errors.
 
 declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            // Defines the props for the <ion-icon> element.
-            // The type is simplified to avoid a dependency on the 'react' module import.
-            // It explicitly types the 'name' prop and allows any other props via an index signature.
-            'ion-icon': {
-                name: string;
-                [key: string]: any;
-            };
-        }
+  namespace JSX {
+    interface IntrinsicElements {
+      // Defines the props for the <ion-icon> element.
+      // The type is simplified to avoid a dependency on the 'react' module import.
+      // It explicitly types the 'name' prop and allows any other props via an index signature.
+      "ion-icon": {
+        name: string;
+        [key: string]: any;
+      };
     }
+  }
 }
 
 // By adding an export, this file is treated as a module, allowing global augmentation.
